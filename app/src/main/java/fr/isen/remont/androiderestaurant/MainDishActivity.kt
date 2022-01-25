edc017798977759dc7eb399d4b3e3de4ff558181
Victoria-Remont <v.remont@free.fr>
2022-01-25 14:49:49 +0100

    Second push : - added second activity

--- app/src/main/java/fr/isen/remont/androiderestaurant/MainDishActivity.kt
+++ app/src/main/java/fr/isen/remont/androiderestaurant/MainDishActivity.kt
@@ -3,10 +3,9 @@ package fr.isen.remont.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainDishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
+        setContentView(R.layout.activity_main_dish)
     }
 }
